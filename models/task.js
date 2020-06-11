88342625f1fc74c31bd1a0efbf0117d3b47b5d1e
IlanitSmul <ilanitsmul@campus.technion.ac.il>
2020-06-11 11:05:14 +0300

    add status field to Task object

--- models/task.js
+++ models/task.js
@@ -2,6 +2,11 @@ var mongoose = require("mongoose");
 
 var taskSchema = mongoose.Schema({
     title: String,
+    status: {
+        type: String,
+        enum: ["Next Up", "In Progress", "Complete"],
+        default: "Next Up"
+    },
     dueDate: String,
     description: String,
 });
